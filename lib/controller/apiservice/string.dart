1b5bff1596ba13ccad3e1b77fe5e86149e1dc539
muhammed safvan <sajusajuptl@gmail.com>
2023-09-30 13:36:01 +0530

    daily weather ui completed

--- lib/controller/apiservice/string.dart
+++ lib/controller/apiservice/string.dart
@@ -2,6 +2,6 @@ const String apiKey = 'd99d638ba389b1f127ee850bc533432b';
 
 String apiUrl(double lat, double long) {
   String url =
-      'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$long&exclude=minutely&appid=$apiKey';
+      "https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$long&exclude=minutely&appid=$apiKey";
   return url;
 }

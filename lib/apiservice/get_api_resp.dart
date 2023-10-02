ee9d420fc095e6dd853d836b184dec6b3c18b923
muhammed safvan <sajusajuptl@gmail.com>
2023-10-02 18:15:51 +0530

    http dependencies to dio

--- lib/apiservice/get_api_resp.dart
+++ lib/apiservice/get_api_resp.dart
@@ -1,26 +1,26 @@
-import 'dart:convert';
 import 'dart:developer';
-import 'package:http/http.dart' as http;
 import 'package:weather_app/apiservice/string.dart';
 import 'package:weather_app/model/current_weatherdata.dart';
 import 'package:weather_app/model/daily_weather_model.dart';
 import 'package:weather_app/model/hourly_weather_model.dart';
 import 'package:weather_app/model/weather_data_model.dart';
+import 'package:dio/dio.dart';
 
 class ApiService {
-  WeatherData? wheatherData;
+  WeatherData? weatherData;
+  final Dio dio = Dio();
 
   Future getWeatherData(lat, lon) async {
     try {
-      var response = await http.get(Uri.parse(apiUrl(lat, lon)));
-      var jsonBody = jsonDecode(response.body);
-      wheatherData = WeatherData(
-        CurrentWeatherData.fromJson(jsonBody),
-        WeatherDataHourly.fromJson(jsonBody),
-        WeatherDataDaily.fromJson(jsonBody),
-      );
+      final response = await dio.get(apiUrl(lat, lon));
       if (response.statusCode == 200 || response.statusCode == 201) {
-        return wheatherData!;
+        final bodyData = response.data;
+        weatherData = WeatherData(
+          CurrentWeatherData.fromJson(bodyData),
+          WeatherDataHourly.fromJson(bodyData),
+          WeatherDataDaily.fromJson(bodyData),
+        );
+        return weatherData!;
       } else {
         log('Server Failure');
       }
